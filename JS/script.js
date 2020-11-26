216bba495d3da85d53c41d29210a89ff533eead6
katherineobide <70817641+obidekatherine@users.noreply.github.com>
2020-11-26 07:41:38 +0100

    new message

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,8 @@ CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
 function reveal() {
-  CTA.classList.toggle("hide");
-  ALERT.classList.toggle("hide");
+	CTA.classList.toggle("hide");
+	ALERT.classList.toggle("hide");
 }
 
 CTA.onclick = reveal;
